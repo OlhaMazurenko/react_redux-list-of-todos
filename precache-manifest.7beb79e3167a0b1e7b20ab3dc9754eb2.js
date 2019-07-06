bb57f1d8a833fe0f944d1a640bdc75f561b73e55
OlhaMazurenko <mail.olgamazurenko@gmail.com>
2019-07-07 02:22:54 +0300

    Updates

---
+++ precache-manifest.7beb79e3167a0b1e7b20ab3dc9754eb2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4c769fbbdd7b56ef0c7b9318a4b5968",
+    "url": "/react_redux-list-of-todos/index.html"
+  },
+  {
+    "revision": "fe74769eaed1024452a9",
+    "url": "/react_redux-list-of-todos/static/css/main.5c089d28.chunk.css"
+  },
+  {
+    "revision": "571c3a24e3947367ffa3",
+    "url": "/react_redux-list-of-todos/static/js/2.25e08047.chunk.js"
+  },
+  {
+    "revision": "fe74769eaed1024452a9",
+    "url": "/react_redux-list-of-todos/static/js/main.a192d9ef.chunk.js"
+  },
+  {
+    "revision": "37dc44999646af065e8e",
+    "url": "/react_redux-list-of-todos/static/js/runtime~main.054cc81e.js"
+  }
+]);
